ceae96f4498adde72c855b9652652bd8ffaa66db
Zachary Forkash <zforkash@optonline.net>
2023-03-29 00:49:06 -0400

    init

---
+++ website/static/only.js
@@ -0,0 +1,8 @@
+function deleteNote(noteId) {
+    fetch("/deleted-note", {
+      method: "POST",
+      body: JSON.stringify({ noteId: noteId }),
+    }).then((_res) => {
+      window.location.href = "/";
+    });
+  }
